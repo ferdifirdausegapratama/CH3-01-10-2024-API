01fe98502e77f71aefbd8c60aee9fa1f45f1d92a
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-10-02 04:25:56 +0700

    update error

--- server.js
+++ server.js
@@ -1,76 +1,77 @@
-const fs = require("fs")
+const fs = require("fs");
 // const http = require("http")
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
-// middleware untuk membaca json dari request body ke kita 
-app.use(express. json())
+// middleware untuk membaca json dari request body(client, FE dll) ke kita
+app.use(express.json());
 
-// default URL = Health Check
+// default URL = Health check
 app.get("/", (req, res) => {
-    res.status(200).json({
-        "status": "Succes",
-        "message": "Application is running good...",
-    })
-})
+  res.status(200).json({
+    status: "Success",
+    message: "Application is running good...",
+  });
+});
 
-// kalau HTTP module kan if(req.url === / "ferdi") {}
-app.get('/ferdi', (req, res) => {
-    res.status(200).json({
-        "message": "Ping Successfully !"
-    })
-})
+// kalau HTTP module kan if(req.url === / "Tegar") {}
+app.get("/tegar", (req, res) => {
+  res.status(200).json({
+    message: "Ping Successfully !",
+  });
+});
 
-// /api/v1/(collection nya) => collectionnya ini harus JAMAK (s)
-app.get('/api/v1/cars', (req, res) => {
-    const cars = JSON.parse(
-        fs.readFileSync(`${__dirname}/data/cars.json`, "utf-8")
-    ); 
-    
-    res.status(200).json({
-        status : "Succes",
-        message: "Succes get cars data",
-        isSucces: true,
-        totalData: cars.length,
-        data: {
-            cars
-        },
-    });
+const cars = JSON.parse(
+  fs.readFileSync(`${__dirname}/assets/data/cars.json`, "utf-8")
+);
+
+// /api/v1/(collection nya) => collection nya ini harus JAMAK (s)
+app.get("/api/v1/cars", (req, res) => {
+  res.status(200).json({
+    status: "Success",
+    message: "Success get cars data",
+    isSuccess: true,
+    totalData: cars.length,
+    data: {
+      cars,
+    },
+  });
 });
 
 // response.data.cars
 
-app.post('/api/v1/cars', (req, res) => {
-    // insert into ....
+app.post("/api/v1/cars", (req, res) => {
+  // insert into ......
+  const newCar = req.body;
 
-    const newCar = req.body;
+  cars.push(newCar);
 
-    cars.push(newCar);
-
-    fs.writeFileSync(`${__dirname}/data/cars.json`, cars, (err) => {
-        
-    cars.status(200).json({
-        status : "Succes",
-        message: "Succes get cars data",
-        isSucces: true,
-        data: cars,
-    });
-
-   
-    });
+  fs.writeFile(
+    `${__dirname}/assets/data/cars.json`,
+    JSON.stringify(cars),
+    (err) => {
+      res.status(201).json({
+        status: "Success",
+        message: "Success add new car data",
+        isSuccess: true,
+        data: {
+          car: newCar,
+        },
+      });
+    }
+  );
 });
 
-// middleware / handler untuk url yang tidak dapat diakses karena memang tidak ada di aplikasi 
-// membuat middlkeware = our own middleware
+// middleware / handler untuk url yang tidak dapat diakses karena memang tidak ada di aplikasi
+// membuat middleware = our own middleware
 app.use((req, res, next) => {
-    res.status(404).json({
-        "status": "Failed",
-        "message": "API not exist !!!"
-    })
-})
+  res.status(404).json({
+    status: "Failed",
+    message: "API not exist !!!",
+  });
+});
 
-app.listen("3000", ()=>{
-    console.log("Start Aplikasi Kita Di Port 3000")
-})
- 
+app.listen("3000", () => {
+  console.log("start aplikasi kita di port 3000");
+});
